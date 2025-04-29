7a947c1f38980e5c64482fb4f8ea9787b7e2a3ae
JM802 <1784927167@qq.cpm>
2025-04-29 17:50:30 +0800

    1

--- greater_factor.cpp
+++ greater_factor.cpp
@@ -1,8 +1,24 @@
-#include<iostream>
-#include<algorithm>
-using namespace std;
-int greater_factor(int m,int n)
-{
-   
+// #include<iostream>
+// #include<algorithm>
+// using namespace std;
+// int greater_factor(int m,int n)
+// {
+//    if(m%n==0)
+//    {
+//        return n;
+//    }
+//    else
+//    {
+//        return greater_factor(n, m % n);
+//    }
+// }
 
-}
+// int main()
+// {
+//     int m, n;
+//     cin >> m >> n;
+//     int i = max(m, n);
+//     int j = min(m, n);
+//     int result = greater_factor(i, j);
+//     cout << result << endl;
+// }
