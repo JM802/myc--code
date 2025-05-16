2a3c6f790ed8aaadd939fe6dd3de258f2e48ce0d
JM802 <1784927167@qq.cpm>
2025-05-17 01:12:33 +0800

    1

--- adjgraph_dfs.cpp
+++ adjgraph_dfs.cpp
@@ -4,6 +4,7 @@
 #include <climits>
 using namespace std;
 
+vector<string> res;
 class arcnode
 {
 public:
@@ -48,8 +49,19 @@ public:
     void addedge(int from, int to, int weight)
     {
         arcnode *newnode = new arcnode(to, weight);
-        newnode->next = hlist[from].firstarc;
-        hlist[from].firstarc = newnode;
+        if (!hlist[from].firstarc)
+        {
+            hlist[from].firstarc = newnode;
+        }
+        else
+        {
+            arcnode *p = hlist[from].firstarc;
+            while (p->next)
+            {
+                p = p->next;
+            }
+            p->next = newnode;
+        }
         e++;
     }
 
@@ -84,6 +96,33 @@ public:
     }
 };
 
+void dfs(adjgraph &g, int v, vector<bool> &visited, vector<string> &path, string &s)
+{
+    if (visited[v])
+    {
+        return;
+    }
+    visited[v] = true;
+    s += g.hlist[v].data;
+    if (s.length() == g.n)
+    {
+        path.push_back(s);
+    }
+    arcnode *p = g.hlist[v].firstarc;
+    while (p)
+    {
+        int w = p->adjver;
+        if (!visited[w])
+        {
+            dfs(g, w, visited, path, s);
+        }
+        p = p->next;
+    }
+    visited[v] = false;
+    s.pop_back();
+    return;
+}
+
 int main()
 {
     int n1;
@@ -96,6 +135,8 @@ int main()
         g.hlist[i].data = da;
     }
     vector<vector<int>> v1(n1, vector<int>(n1));
+    vector<bool> visited(n1, false);
+    vector<string> path;
     for (int i = 0; i < n1; i++)
     {
         for (int j = 0; j < n1; j++)
@@ -106,6 +147,11 @@ int main()
         }
     }
     g.creategraph(v1);
-    g.display();
+    string currentPath = "";
+    dfs(g, 0, visited, path, currentPath);
+    for (const auto &m : path)
+    {
+        cout << m << endl;
+    }
     return 0;
 }
